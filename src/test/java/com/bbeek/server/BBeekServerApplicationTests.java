35ee54d58320123618190213c800353e1cd219b9
YunChan-Oh <oyunchan9@gmail.com>
2024-10-29 17:16:49 +0900

    init

---
+++ src/test/java/com/bbeek/server/BBeekServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbeek.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BBeekServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
