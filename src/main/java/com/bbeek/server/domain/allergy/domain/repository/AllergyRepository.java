566cec757351b6124522e3e11d5125f77a3d3159
YunChan-Oh <oyunchan9@gmail.com>
2024-10-30 11:34:23 +0900

    feat(allergy): AllergyRepository

---
+++ src/main/java/com/bbeek/server/domain/allergy/domain/repository/AllergyRepository.java
@@ -0,0 +1,9 @@
+package com.bbeek.server.domain.allergy.domain.repository;
+
+import com.bbeek.server.domain.allergy.domain.Allergy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AllergyRepository extends JpaRepository<Allergy, Long> {
+}
